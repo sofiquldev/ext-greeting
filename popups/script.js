322698a66e6d0acd2041ad7a767aaea24487ab1a
sofiquldev <sofiquldev@gmail.com>
2022-10-31 17:20:15 +0600

    first commit

---
+++ popups/script.js
@@ -0,0 +1,10 @@
+let greeting = {
+    title: "Good s Morning!",
+    date: "17th Dec, 2022",
+}
+
+let quote = {
+    content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius, beatae. Placeat laboriosam sunt molestias quam",
+    user: "Ainsine"
+}
+
